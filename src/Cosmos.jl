0f3dba597060871519a29d31ad0ec0eb2a1878d5
Rafael <rafaelab@users.noreply.github.com>
2022-05-27 14:49:20 +0200

    add dependencies

--- src/Cosmos.jl
+++ src/Cosmos.jl
@@ -1,11 +1,35 @@
 module Cosmos
 
+export 
+	CosmologicalModel,
+	CosmologyPlanck,
+	CosmologyCRPropa,
+	redshiftToComovingDistance,
+	redshiftToLuminosityDistance,
+	redshiftToLightTravelDistance,
+	redshiftToLookbackTime,
+	redshiftsToComovingDistance,
+	redshiftsToLightTravelDistance,
+	comovingDistanceToRedshift,
+	lightTravelDistanceToRedshift,
+	luminosityDistanceToRedshift
 
+using Interpolations
+using Longinus.Scales
 using Reexport
 @reexport using Cosmology
 @reexport using PhysicalConstants.CODATA2014
 @reexport using Unitful
 @reexport using UnitfulAstro
+
+import Longinus.Miscellaneous: Maybe, AbstractVectorOrNTuple
+import Unitful: 𝐋
+
+
+
+include("cosmology.jl")
+
+
 
 
 end # module
