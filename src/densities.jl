9fc0460e55f2ca7bc483e678c4b3806e004bdd1c
Rafael <rafaelab@users.noreply.github.com>
2023-05-21 16:07:26 -0400

    first commit

---
+++ src/densities.jl
@@ -0,0 +1,89 @@
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	computeCriticalDensity(cosmology; z = 0.)
+
+Computes the critical density of the universe at a given redshift:
+  ρc = 3 H^2(z) / 8π G
+
+# Input 
+. `cosmology::CosmologicalModel`: the cosmological model of interest \\
+. `z::Real`: the redshift at which to compute the density \\
+"""
+function computeCriticalDensity(cosmology::CosmologicalModel; z::Real = 0.)
+	return upreferred(3. * hubbleParameter(cosmology, z) ^ 2 / 8π / G)
+end
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	computeMatterDensity(cosmology; z = 0.)
+
+Computes the density of matter in the universe at a given redshift:
+  ρm = Ωm * ρc
+
+# Input 
+. `cosmology::CosmologicalModel`: the cosmological model of interest \\
+. `z::Real`: the redshift at which to compute the density \\
+"""
+function computeMatterDensity(cosmology::CosmologicalModel; z::Real = 0.)
+	ρc = computeCriticalDensity(cosmology; z = z)
+	return upreferred(cosmology.Ωm * ρc)
+end
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	computeRadiationDensity(cosmology; z = 0.)
+
+Computes the density of matter in the universe at a given redshift:
+  ρr = Ωr * ρc
+
+# Input 
+. `cosmology::CosmologicalModel`: the cosmological model of interest \\
+. `z::Real`: the redshift at which to compute the density \\
+"""
+function computeRadiationDensity(cosmology::CosmologicalModel; z::Real = 0.)
+	ρc = computeCriticalDensity(cosmology; z = z)
+	return upreferred(cosmology.Ωr * ρc)
+end
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	computeCurvatureDensity(cosmology; z = 0.)
+
+Computes the density of matter in the universe at a given redshift:
+  ρk = Ωk * ρc
+
+# Input 
+. `cosmology::CosmologicalModel`: the cosmological model of interest \\
+. `z::Real`: the redshift at which to compute the density \\
+"""
+function computeCurvatureDensity(cosmology::CosmologicalModel; z::Real = 0.)
+	ρc = computeCriticalDensity(cosmology; z = z)
+	return upreferred(cosmology.Ωk * ρc)
+end
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	computeDarkEnergyDensity(cosmology; z = 0.)
+
+Computes the density of matter in the universe at a given redshift:
+  ρΛ = ΩΛ * ρc
+
+# Input 
+. `cosmology::CosmologicalModel`: the cosmological model of interest \\
+. `z::Real`: the redshift at which to compute the density \\
+"""
+function computeDarkEnergyDensity(cosmology::CosmologicalModel; z::Real = 0.)
+	ρc = computeCriticalDensity(cosmology; z = z)
+	return upreferred(cosmology.ΩΛ * ρc)
+end
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
