b8ecf8c282f4ae3fe5f51193aee282009da1e797
Rafael <rafaelab@users.noreply.github.com>
2023-05-26 09:28:31 -0400

    add helper functions for

--- src/common.jl
+++ src/common.jl
@@ -8,3 +8,33 @@ const Maybe{T} = Union{Nothing, T}
 
 
 # ----------------------------------------------------------------------------------------------- #
+# 
+for cosmologyType in ("FlatLCDM", "OpenLCDM", "ClosedLCDM", "FlatWCDM", "OpenWCDM", "ClosedWCDM")
+	@eval begin
+		@doc """
+		Useful extensions of Cosmology.jl to enable type conversion related to `$($(Symbol("$(cosmologyType)")))`.
+		"""
+		function Base.convert(::Type{T}, cosmo::$(Symbol("$(cosmologyType)")){U}) where {T <: Real, U} 
+			fields = fieldnames(typeof(cosmo))
+			values = Tuple([convert(T, getfield(cosmo, field)) for field in fields])
+			return $(Symbol("$(cosmologyType)")){T}(values...)
+		end
+
+		Base.convert(::Type{$(Symbol("$(cosmologyType)")){T}}, cosmo::$(Symbol("$(cosmologyType)")){U}) where {T, U} = convert(T, cosmo)
+
+		@doc """
+		Useful extensions of Cosmology.jl implementing promotion rules.
+		"""
+		Base.promote_rule(::Type{$(Symbol("$(cosmologyType)")){T}}, ::Type{$(Symbol("$(cosmologyType)")){U}}) where {T, U} = $(Symbol("$(cosmologyType)")){promote_type(T, U)}
+	end
+end
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+Get the underlying data type of an `AbstractCosmology` object (from Cosmology.jl).
+"""
+Base.eltype(cosmo::AbstractCosmology) = typeof(cosmo.h)
+
+
+# ----------------------------------------------------------------------------------------------- #
