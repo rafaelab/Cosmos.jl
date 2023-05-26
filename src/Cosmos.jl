f5d59ef63e951ea28e26b6afbdf8cf7cab13ed62
Rafael <rafaelab@users.noreply.github.com>
2023-05-26 09:28:03 -0400

    update for redesign

--- src/Cosmos.jl
+++ src/Cosmos.jl
@@ -3,53 +3,72 @@ module Cosmos
 export 
 	CosmologicalModel,
 	CosmologyPlanck,
+	Redshift,
+	DistanceLightTravel,
+	DistanceComoving,
+	DistanceLuminosity,
+	DistanceAngularDiameter,
+	DistanceTransverseComoving,
+	# TimeLookback,
+	# TimeConformal,
+	isFlat,
+	isOpen,
+	isClosed,
+	isWarm,
+	isCold,
 	scaleFactor,
 	ageOfUniverse,
 	hubbleConstant,
 	hubbleDistance,
 	hubbleTime,
 	hubbleParameter,
-	dimensionlessHubbleParameter,
-	redshiftToComovingDistance,
-	redshiftToLuminosityDistance,
-	redshiftToLightTravelDistance,
-	redshiftToAngularDiameterDistance,
-	redshiftToTransverseComovingDistance,
-	redshiftToLookbackTime,
-	comovingDistanceToRedshift,
-	lightTravelDistanceToRedshift,
-	luminosityDistanceToRedshift,
-	comovingTransverseDistanceToRedshift,
-	angularDiameterDistanceToRedshift,
-	lightTravelToComovingDistance,
-	comovingVolume,
-	comovingVolumeElement,
-	comovingElement,
-	calculateDensityPhotons,
-	calculateDensityNeutrinos,
-	computeCriticalDensity,
-	computeMatterDensity,
-	computeDarkEnergyDensity,
-	computeCurvatureDensity,
-	computeRadiationDensity
+	dimensionlessHubbleParameter
+	# redshiftToComovingDistance,
+	# redshiftToLuminosityDistance,
+	# redshiftToLightTravelDistance,
+	# redshiftToAngularDiameterDistance,
+	# redshiftToTransverseComovingDistance,
+	# redshiftToLookbackTime,
+	# comovingDistanceToRedshift,
+	# lightTravelDistanceToRedshift,
+	# luminosityDistanceToRedshift,
+	# comovingTransverseDistanceToRedshift,
+	# angularDiameterDistanceToRedshift,
+	# lightTravelToComovingDistance,
+	# comovingVolume,
+	# comovingVolumeElement,
+	# comovingElement,
+	# calculateDensityPhotons,
+	# calculateDensityNeutrinos,
+	# computeCriticalDensity,
+	# computeMatterDensity,
+	# computeDarkEnergyDensity,
+	# computeCurvatureDensity,
+	# computeRadiationDensity
 
 
-using Cosmology
 using Interpolations
+using Printf
 using Reexport
 @reexport using PhysicalConstants.CODATA2018
 @reexport using Unitful
 @reexport using UnitfulAstro
 
-import Cosmology: OpenLCDM, OpenWCDM, ClosedLCDM, ClosedWCDM, FlatLCDM, FlatWCDM
+import Cosmology
 import Cosmology: H, E
+import Cosmology: AbstractCosmology
+import Cosmology: OpenLCDM, ClosedLCDM, FlatLCDM
+import Cosmology: OpenWCDM, ClosedWCDM, FlatWCDM
+import Cosmology: angular_diameter_dist, comoving_radial_dist, comoving_transverse_dist, luminosity_dist, lookback_time
 
 
-include("common.jl")
+include("common.jl") 
+include("redshift.jl")
 include("cosmologicalModel.jl")
 include("cosmology.jl")
-include("measures.jl")
+include("distanceTime.jl")
 include("densities.jl")
+include("io.jl")
 
 
 end # module
--- src/cosmologicalModel.jl
+++ src/cosmologicalModel.jl
@@ -2,43 +2,77 @@
 #
 @doc """
 General struct to hold a `Cosmology.jl` object of type `AbstractCosmology`.
-The only member of this struct that should be accessed is cosmology. 
-The others are only useful for the functions.
-
-Any cosmology can be built using the following parameters:
-. `h`: dimensionless Hubble constant \\
-. `Ωm`: matter density \\
-. `Ωr`: radiation density \\
-. `Ωk`: curvature density \\
-. `ΩΛ`: dark energy density \\
-. `Ωb`: baryon density (set to -1 if unavailable) \\
-. `wEOSΛ`: tuple with parameters of the equation of state for dark energy: `w = w_0 + w_a (1 - a)` \\
-
+It exposes the relevant cosmological parameters and adds new ones.
+The equation of state follows the Chevallier-Polarski-Linder parametrisation: \\
+	"Accelerating Universes with Scaling Dark Matter" \\
+	M. Chevallier and D. Polarski \\
+	International Journal of Modern Physics D 10 (2001) 213. \\
+	https://arxiv.org/abs/gr-qc/0009008 \\
+	https://doi.org/10.1142/S0218271801000822 \\
+	\\
+	"Exploring the Expansion History of the Universe" \\
+	E. Linder \\
+	Physical Review Letters 90 (2003) 091301. \\
+	https://doi.org/10.1103/PhysRevLett.90.091301 \\
+	\\
 The default constructors can be built using only the first 3 or 4 parameters.
+
+# Members
+. `h::Real`: dimensionless Hubble constant \\
+. `Ωm::Real`: matter density \\
+. `Ωr::Real`: radiation density \\
+. `Ωk::Real`: curvature density \\
+. `ΩΛ::Real`: dark energy density \\
+. `Ωb::Real`: baryon density (set to -1 if unavailable) \\
+. `Nν::Real`: number of effective neutrino species (defaults to 3.04, following Planck) \\
+. `Tcmb::Real`: CMB temperature at present time (defaults to 2.7255 K, following Planck) \\
+. `wEOSΛ::NTuple{2, Real}`: tuple with parameters of the equation of state for dark energy: `w = w_0 + w_a (1 - a)` \\
+. `cosmology::AbstractCosmology`: object from `Cosmology.jl` \\
+. `toRedshift::Dict{Symbol, Function}`: functions to convert distance/time to redshift (`:comoving`, `:lightTravel`, `:angularDiameter`,`:transverseComoving`, `:luminosity`, `:lookback`, `:conformal`) \\
+. `fromRedshift::Dict{Symbol, Function}`: functions to convert distance/time from redshift (`:comoving`, `:lightTravel`, `:angularDiameter`, `:transverseComoving`, `:luminosity`, `:lookback`, `:conformal`) \\
+. `zArray::Maybe{AbstractVector}`: array of values of redshift to build distance/time conversion functions; if nothing defaults to built-in values \\
+
+# Examples
+```
+	# define parameters
+	Tcmb = 2.7255
+	h = 0.69
+	ΩΛ = 0.7099
+	Ωk = 0.
+	Ωm = 0.29
+	Ωr = 1. - ΩΛ - Ωk - Ωm
+	Nν = 3.04
+	
+	# some constructors
+	cosmo1 = CosmologicalModel(Cosmology.FlatLCDM{Float64}(h, ΩΛ, Ωm, Ωr); Nν = Nν, Tcmb = Tcmb)
+	cosmo2 = CosmologicalModel(h, Ωm; Tcmb = Tcmb,  Nν = Nν) # assumes Ωr = 0
+	cosmo3 = CosmologicalModel(h, Ωm, Ωk; Tcmb = Tcmb,  Nν = Nν) # if geometry is not flat and Ωr = 0
+	cosmo4 = CosmologicalModel(h, Ωm, Ωk, Ωr; Tcmb = Tcmb,  Nν = Nν) # includes radiation and non-flat geometry 
+
+```
 """
-mutable struct CosmologicalModel{C <: Cosmology.AbstractCosmology, T <: AbstractFloat}
+mutable struct CosmologicalModel{C <: AbstractCosmology, T <: Real}
 	h::T
 	ΩΛ::T
 	Ωm::T
 	Ωr::T
 	Ωk::T
 	Ωb::T
+	Tcmb::T
+	Nν::T
 	wEOSΛ::Tuple{T, T}
-	_cosmology::C
-	_comovingDistance2Redshift
-	_luminosityDistance2Redshift
-	_lightTravelDistance2Redshift
-	_comovingTransverse2Redshift
-	_angularDiameterDistance2Redshift
-	function CosmologicalModel{C, T}(cosmo::C; z::Union{Nothing, Vector{Z}} = nothing, Ωb::Real = -1.) where {C <: Cosmology.AbstractCosmology, T <: Real, Z <: Real}		
+	cosmology::C
+	toRedshift::Dict{Symbol, Function}
+	fromRedshift::Dict{Symbol, Function}
+	zArray::Vector{T}
+	function CosmologicalModel{C, T}(cosmo::C; Ωb::Real = -1., Tcmb::Real = 2.7255, Nν::Real = 3., z::Maybe{AbstractVector} = nothing) where {C <: AbstractCosmology, T <: Real}		
 		h = cosmo.h
-		Ωk = (C isa Union{Cosmology.AbstractClosedCosmology, Cosmology.AbstractOpenCosmology}) ? cosmo.Ω_k : zero(T)
 		Ωr = cosmo.Ω_r
 		Ωm = cosmo.Ω_m
 		ΩΛ = cosmo.Ω_Λ
-		wEOSΛ = (C isa Union{FlatWCDM, OpenWCDM, ClosedWCDM}) ? (cosmo.w0, cosmo.wa) : (- one(T), zero(T))
-		
-		# prepare redshifts
+		Ωk = (C isa Union{Cosmology.AbstractClosedCosmology, Cosmology.AbstractOpenCosmology}) ? cosmo.Ω_k : zero(T)
+		wEOSΛ = (C isa Union{Cosmology.FlatWCDM, Cosmology.OpenWCDM, Cosmology.ClosedWCDM}) ? (cosmo.w0, cosmo.wa) : (- one(T), zero(T))
+
 		if isnothing(z)
 			z = T[]
 			append!(z, -10. .^ collect(range(-3., 0.; length = 91)))
@@ -47,50 +81,22 @@ mutable struct CosmologicalModel{C <: Cosmology.AbstractCosmology, T <: Abstract
 			append!(z, collect(range(1., 2.; length = 21)))
 			append!(z, 10 .^ collect(range(2., 4.; length = 41)))
 			unique!(z)
-		else
-			z = convert(Vector{T}, z)
 		end
-		z = z[z .> -1.0]
-		sort!(z)
 
-		# distance arrays
-		dC, dL, dP, dT, dA = T[], T[], T[], T[], T[]
+		funcTo = conversionsToRedshift(cosmo, z)
+		funcFrom = conversionsFromRedshift(cosmo)
 
-		@simd for i in eachindex(z)
-			dC0 = comoving_radial_dist(cosmo, z[i]) |> u"m"
-			dL0 = luminosity_dist(cosmo, z[i]) |> u"m"
-			dP0 = (lookback_time(cosmo, z[i]) |> u"s") * SpeedOfLightInVacuum |> u"m"
-			dT0 = comoving_transverse_dist(cosmo, z[i]) |> u"m"
-			dA0 = angular_diameter_dist(cosmo, z[i]) |> u"m"
-			@inbounds push!(dC, ustrip.(dC0 |> u"m"))
-			@inbounds push!(dL, ustrip.(dL0 |> u"m"))
-			@inbounds push!(dP, ustrip.(dP0 |> u"m"))
-			@inbounds push!(dT, ustrip.(dT0 |> u"m"))
-			@inbounds push!(dA, ustrip.(dA0 |> u"m"))
-		end
-	
-		# get indices that sort arrays, as required by Interpolations.jl
-		idxC = sortperm(dC)
-		idxL = sortperm(dL)
-		idxP = sortperm(dP)
-		idxT = sortperm(dT)
-		idxA = sortperm(dA)
-	
-		# functions: for `Interpolations` version < 0.14
-		# c2z = LinearInterpolation(ustrip.(dC[idxC]), z[idxC])
-		# l2z = LinearInterpolation(ustrip.(dL[idxL]), z[idxL])
-		# p2z = LinearInterpolation(ustrip.(dP[idxP]), z[idxP])
-
-		# extrap_full = extrapolate(scale(interpolate(A, BSpline(Linear())), xs), Line())
-
-		c2z = interpolate(dC[idxC], z[idxC], SteffenMonotonicInterpolation())
-		l2z = interpolate(dL[idxL], z[idxL], SteffenMonotonicInterpolation())
-		p2z = interpolate(dP[idxP], z[idxP], SteffenMonotonicInterpolation())
-		t2z = interpolate(dT[idxT], z[idxT], SteffenMonotonicInterpolation())
-		a2z = interpolate(dA[idxA], z[idxA], SteffenMonotonicInterpolation())
-
-		return new{typeof(cosmo), typeof(h)}(h, ΩΛ, Ωm, Ωr, Ωk, Ωb, wEOSΛ, cosmo, c2z, l2z, p2z, t2z, a2z)
+		return new{C, T}(T(h), T(ΩΛ), T(Ωm), T(Ωr), T(Ωk), T(Ωb), T(Tcmb), T(Nν), T.(wEOSΛ), convert(T, cosmo), funcTo, funcFrom, z)
 	end
+end
+
+function CosmologicalModel{T}(cosmo::C; args...) where {C <: AbstractCosmology, T <: Real}	
+	c = convert(T, cosmo)
+	return CosmologicalModel{typeof(c), T}(c; args...)
+end
+
+function CosmologicalModel(cosmo::C; args...) where {C <: AbstractCosmology}
+	return CosmologicalModel{C, eltype(cosmo.cosmology)}(cosmo; args...)
 end
 
 function CosmologicalModel(h::Real, Ωm::Real; args...)
@@ -109,21 +115,234 @@ function CosmologicalModel(h::Real, Ωm::Real, Ωk::Real, Ωr::Real; args...)
 end
 
 
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+Helper functions to iniatilise conversions of distance or time to/from redshiftl.
+"""
+function conversionsFromRedshift(cosmo::AbstractCosmology)
+	T = eltype(cosmo)
+
+	z2dl(z2::Real, z1::Real) = T(luminosity_dist(cosmo, z2) - luminosity_dist(cosmo, z1))
+	z2dc(z2::Real, z1::Real) = T(comoving_radial_dist(cosmo, z2, z1))
+	z2dt(z2::Real, z1::Real) = T(transverse_comoving_dist(cosmo, z2, z1))
+	z2da(z2::Real, z1::Real) = T(angular_diameter_dist(cosmo, z1, z2))
+	z2tl(z2::Real, z1::Real) = T(lookback_time(cosmo, z2) - lookback_time(cosmo, z1) )
+	z2tc(z2::Real, z1::Real) = T(z2dc(z2, z1) / SpeedOfLightInVacuum |> u"yr")
+	z2dp(z2::Real, z1::Real) = T(z2tl(z2, z1) * SpeedOfLightInVacuum |> u"Mpc")
+
+	funcDict = Dict()
+	funcDict[:comoving] = z2dc
+	funcDict[:lightTravel] = z2dp
+	funcDict[:luminosity] = z2dl
+	funcDict[:transverseComoving] = z2dt
+	funcDict[:angularDiameter] = z2da
+	funcDict[:lookback] = z2tl
+	funcDict[:conformal] = z2tc
+
+	return funcDict
+end
+
+function conversionsToRedshift(cosmo::AbstractCosmology, z::AbstractVector)
+	T = eltype(cosmo)
+
+	# prepare redshifts
+	if isnothing(z)
+		z = T[]
+		append!(z, -10. .^ collect(range(-3., 0.; length = 91)))
+		append!(z, collect(range(-0.001, 0.001; length = 31)))
+		append!(z, collect(range(0.001, 10.; length = 81)))
+		append!(z, collect(range(1., 2.; length = 21)))
+		append!(z, 10 .^ collect(range(2., 4.; length = 41)))
+		unique!(z)
+ 	else
+		z = convert(Vector{T}, z)
+	end
+	z = z[z .> -1.0]
+	sort!(z)
+
+	# distance arrays
+	dC, dL, dP, dT, dA = T[], T[], T[], T[], T[]
+	tC, tL = T[], T[]
+
+	@simd for i in eachindex(z)
+		dC0 = comoving_radial_dist(cosmo, z[i])
+		dL0 = luminosity_dist(cosmo, z[i]) 
+		dT0 = comoving_transverse_dist(cosmo, z[i])
+		dA0 = angular_diameter_dist(cosmo, z[i])
+		tL0 = lookback_time(cosmo, z[i])
+		dP0 = tL0 * SpeedOfLightInVacuum
+		tC0 = dC0 / SpeedOfLightInVacuum
+		@inbounds push!(dC, ustrip.(dC0 |> u"m"))
+		@inbounds push!(dL, ustrip.(dL0 |> u"m"))
+		@inbounds push!(dP, ustrip.(dP0 |> u"m"))
+		@inbounds push!(dT, ustrip.(dT0 |> u"m"))
+		@inbounds push!(dA, ustrip.(dA0 |> u"m"))
+		@inbounds push!(tL, ustrip.(tL0 |> u"s"))
+		@inbounds push!(tC, ustrip.(tC0 |> u"s"))
+	end
+
+	# get indices that sort arrays, as required by Interpolations.jl
+	idxDC = sortperm(dC)
+	idxDL = sortperm(dL)
+	idxDP = sortperm(dP)
+	idxDT = sortperm(dT)
+	idxDA = sortperm(dA)
+	idxTC = sortperm(tC)
+	idxTL = sortperm(tL)
+
+	dc2z_ = interpolate(dC[idxDC], z[idxDC], SteffenMonotonicInterpolation())
+	dl2z_ = interpolate(dL[idxDL], z[idxDL], SteffenMonotonicInterpolation())
+	dp2z_ = interpolate(dP[idxDP], z[idxDP], SteffenMonotonicInterpolation())
+	dt2z_ = interpolate(dT[idxDT], z[idxDT], SteffenMonotonicInterpolation())
+	da2z_ = interpolate(dA[idxDA], z[idxDA], SteffenMonotonicInterpolation())
+	tl2z_ = interpolate(tL[idxTL], z[idxTL], SteffenMonotonicInterpolation())
+	tc2z_ = interpolate(tC[idxTC], z[idxTC], SteffenMonotonicInterpolation())
+
+	dc2z(x::Real) = dc2z_(x)
+	dl2z(x::Real) = dl2z_(x)
+	dp2z(x::Real) = dp2z_(x)
+	dt2z(x::Real) = dt2z_(x)
+	da2z(x::Real) = da2z_(x)
+	tl2z(x::Real) = tl2z_(x)
+	tc2z(x::Real) = tc2z_(x)
+	dc2z(x::Unitful.Length) = dc2z_(ustrip(x |> u"m"))
+	dl2z(x::Unitful.Length) = dl2z_(ustrip(x |> u"m"))
+	dp2z(x::Unitful.Length) = dp2z_(ustrip(x |> u"m"))
+	dt2z(x::Unitful.Length) = dt2z_(ustrip(x |> u"m"))
+	da2z(x::Unitful.Length) = da2z_(ustrip(x |> u"m"))
+	tl2z(x::Unitful.Length) = tl2z_(ustrip(x |> u"m"))
+	tc2z(x::Unitful.Length) = tc2z_(ustrip(x |> u"m"))
+
+	funcDict = Dict()
+	funcDict[:comoving] = dc2z
+	funcDict[:lightTravel] = dp2z
+	funcDict[:luminosity] = dl2z
+	funcDict[:transverseComoving] = dt2z
+	funcDict[:angularDiameter] = da2z
+	funcDict[:lookback] = tl2z
+	funcDict[:conformal] = tc2z
+
+	return funcDict
+end
 
 # ----------------------------------------------------------------------------------------------- #
 # 
 @doc """
 Latest Planck's cosmology.
+This is based on:
+  "Planck 2018 results. VI. Cosmological parameters"
+  Planck Collaboration
+  Astronomy and Astrophysics 641 (2020) A6.
+  https://arxiv.org/abs/1807.06209
+  https://doi.org/10.1051/0004-6361/201833910
+
+# Input
+. `z::Maybe{AbstractVector}`: vectors at which the redshifts will be sampled (for interpolating distance measures) \\
+. `T::Type`: type of the data (defaults to `Float64`) \\
 """
-const CosmologyPlanck(; z::Union{Nothing, Vector{Z}} = nothing) where {Z <: Real} = CosmologicalModel{typeof(cosmology()), Float64}(cosmology(); z = z, Ωb = 0.023)
+function CosmologyPlanck(; T::Type = Float64)
+	cosmo0 = Cosmology.cosmology()
+	cosmo = Cosmology.FlatLCDM{T}(T(cosmo0.h), T(cosmo0.Ω_Λ), T(cosmo0.Ω_m), T(cosmo0.Ω_r))
+	return CosmologicalModel{typeof(cosmo), T}(cosmo; Ωb = T(0.023), Nν = T(3.04), Tcmb = T(2.7255))
+end
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	isFlat(cosmology)
+
+Determine whether a `CosmologicalModel` has a flat geometry.
+
+# Input
+. `cosmol::CosmologicalModel`: the cosmological model
+"""
+isFlat(cosmology::CosmologicalModel) = cosmology.cosmology isa Cosmology.AbstractFlatCosmology
 
 
 # ----------------------------------------------------------------------------------------------- #
 # 
-# Overload Base functions. 
-# The returned type is essentially the data type of the underlying cosmology from `Cosmology.jl` (Float64, by default).
-Base.eltype(cosmo::CosmologicalModel) = typeof(cosmo._cosmology.h)
+@doc """
+	isOpen(cosmology)
 
+Determine whether a `CosmologicalModel` has an open geometry.
+
+# Input
+. `cosmol::CosmologicalModel`: the cosmological model
+"""
+isOpen(cosmology::CosmologicalModel) = cosmology.cosmology isa Cosmology.AbstractOpenCosmology
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	isClosed(cosmology)
+
+Determine whether a `CosmologicalModel` has a closed geometry.
+
+# Input
+. `cosmol::CosmologicalModel`: the cosmological model
+"""
+isClosed(cosmology::CosmologicalModel) = cosmology.cosmology isa Cosmology.AbstractClosedCosmology
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	isCold(cosmology)
+
+Determine whether a `CosmologicalModel` is described by cold dark matter.
+
+# Input
+. `cosmol::CosmologicalModel`: the cosmological model
+"""
+isCold(cosmology::CosmologicalModel) = (cosmology.cosmology isa Cosmology.FlatLCDM) || (cosmology.cosmology isa Cosmology.OpenLCDM) || (cosmology.cosmology isa Cosmology.ClosedLCDM)
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+	isWarm(cosmology)
+
+Determine whether a `CosmologicalModel` is described by warm dark matter.
+
+# Input
+. `cosmol::CosmologicalModel`: the cosmological model
+"""
+isWarm(cosmology::CosmologicalModel) = (cosmology.cosmology isa Cosmology.FlatWCDM) || (cosmology.cosmology isa Cosmology.OpenWCDM) || (cosmology.cosmology isa Cosmology.ClosedWCDM)
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+Get type of values contained in `CosmologicalModel` object.
+"""
+Base.eltype(cosmology::CosmologicalModel) = typeof(cosmology.h)
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+Object equality comparison.
+"""
+Base.:(==)(cosmol1::CosmologicalModel{C1, T1}, cosmol2::CosmologicalModel{C2, T2}) where {C1, C2, T1, T2} = (T1 == T2) && (C1 == C2) && (cosmol1.cosmology == cosmol2.cosmology) && (cosmol1.Ωb == cosmol2.Ωb) && (cosmol1.Nν == cosmol2.Nν) && (cosmol1.wEOSΛ .== cosmol1.wEOSΛ)
+
+Base.:(!=)(cosmol1::CosmologicalModel{C1, T1}, cosmol2::CosmologicalModel{C2, T2}) where {C1, C2, T1, T2} = !(cosmol1 == cosmol2)
+
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+Type conversion.
+"""
+Base.convert(::Type{T}, cosmology::CosmologicalModel{C, U}) where {C, T <: Real, U} = CosmologicalModel{typeof(convert(T, cosmology.cosmology)), T}(convert(T, cosmology.cosmology); Ωb = cosmology.Ωb, Nν = cosmology.Nν, Tcmb = cosmology.Tcmb)
+
+# ----------------------------------------------------------------------------------------------- #
+# 
+@doc """
+Promotion rules.
+"""
+Base.promote_rule(::Type{CosmologicalModel{C, T1}}, ::Type{CosmologicalModel{C, T2}}) where {C, T1, T2} = CosmologicalModel{C, promote_type(T1, T2)}
 
 
 # ----------------------------------------------------------------------------------------------- #
