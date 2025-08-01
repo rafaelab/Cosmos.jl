dcd21bc928a4532a658d51c86637dd67bd553a7c
Rafael <rafaelab@users.noreply.github.com>
2025-08-01 13:23:48 +0200

    improve doc generation

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,25 @@
 using Documenter
 
-push!(LOAD_PATH, "..")
-
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
 using Cosmos
 
 DocMeta.setdocmeta!(Cosmos, :DocTestSetup, :(using Cosmos))
 
-
-makedocs(
+makedocs(;
 	sitename = "Cosmos.jl",
+	authors = "Rafael Alves Batista",
 	format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
 	modules = [Cosmos],
 	workdir = joinpath(@__DIR__, ".."),
 	pages = [
 		"Home" => "index.md",
-	]
+		],
+	doctest = true,
 )
 
-deploydocs(repo = "github.com/rafaelab/Cosmos.jl.git", versions = nothing)
+
+deploydocs(;
+	repo = "github.com/rafaelab/Cosmos.jl.git",
+	branch = "gh-pages",
+	versions = nothing
+)
